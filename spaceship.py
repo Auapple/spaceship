4155816c19bcec31e8e3466af2e52ca56d64ed9b
Auapple <76393258+Auapple@users.noreply.github.com>
2020-12-21 22:18:17 +0800

    picture find
    picture find

--- spaceship.py
+++ spaceship.py
@@ -1,1 +1,21 @@
 import pygame as pg, random, math
+pg.init()
+
+screen = pg.display.set_mode([1400,800])
+pg.display.set_caption('galaxy spaceship')
+bg = pg.image.load("bestgalaxy.png")
+bg = pg.transform.scale(bg,(1400,800))
+screen.blit(bg, (0,0))
+
+class Bird(pg.sprite.Sprite):
+	x = 50
+	y = 350
+	speed = 0
+
+	def __init__(self):
+		pg.sprite.Sprite.__init__(self)
+		self.image = pg.image.load("ship.png")
+		self.image = pg.transform.scale(self.image,(100,85))
+		self.rect = self.image.get_rect()
+		self.rect.x = 50
+		self.rect.y = 350
