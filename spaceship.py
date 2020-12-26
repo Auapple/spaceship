d4fa3e5aacf739cff278097247fd10f52969646c
Auapple <76393258+Auapple@users.noreply.github.com>
2020-12-26 11:28:38 +0800

    more codin
    adjust and add more pictures
    coded bullets, fireball, and enemy ship

--- spaceship.py
+++ spaceship.py
@@ -16,7 +16,7 @@ class Ship(pg.sprite.Sprite):
 
 	def __init__(self):
 		pg.sprite.Sprite.__init__(self)
-		self.image = pg.image.load("ship.jpg")
+		self.image = pg.image.load("ship.png")
 		self.image = pg.transform.scale(self.image,(126,94))
 		self.rect = self.image.get_rect()
 		self.rect.x = 50
@@ -31,11 +31,13 @@ class Ship(pg.sprite.Sprite):
 		self.rect.y += self.speed
 
 
+
+
 class Rocket(pg.sprite.Sprite):
 
 	def __init__(self,y):
 		pg.sprite.Sprite.__init__(self)
-		self.image = pg.image.load("rocket.jfif")
+		self.image = pg.image.load("rocket.png")
 		self.image = pg.transform.scale(self.image,(100,60))
 		self.x = 1450
 		self.speed = 20
@@ -81,22 +83,136 @@ class Rock(pg.sprite.Sprite):
 	def update(self):
 		self.rect.x -= self.speed
 
+class Bullet(pg.sprite.Sprite):
+
+	def __init__(self,y):
+		pg.sprite.Sprite.__init__(self)
+		self.image = pg.image.load('bullet.png')
+		self.image = pg.transform.scale(self.image,(37,17))
+		self.x =146
+		self.y = shipy
+		self.speed = 30
+		self.rect = self.image.get_rect()
+		self.rect.x = 146
+		self.rect.y = shipy + 38
+
+	def update(self):
+		self.rect.x += self.speed
+
+
+class Redship(pg.sprite.Sprite):
+
+	def __init__(self,y):
+		pg.sprite.Sprite.__init__(self)
+		self.image = pg.image.load('enemyship.png')
+		self.image = pg.transform.scale(self.image,(140,126))
+		self.x = 1450
+		self.y = redshipy
+		self.speed = 5
+		self.rect = self.image.get_rect()
+		self.rect.x = 1450
+		self.rect.y = redshipy
+
+	def update(self):
+		self.rect.x -= self.speed
+
+
+class Grayship(pg.sprite.Sprite):
+
+	def __init__(self,y):
+		pg.sprite.Sprite.__init__(self)
+		self.image = pg.image.load('othership.png')
+		self.image = pg.transform.scale(self.image,(140,140))
+		self.x = 1450
+		self.y = grayshipy
+		self.speed = 8
+		self.rect = self.image.get_rect()
+		self.rect.x = 1450
+		self.rect.y = grayshipy
+
+	def update(self):
+		self.rect.x -= self.speed
+
+
+class Redbiu(pg.sprite.Sprite):
+
+	def __init__(self,y):
+		pg.sprite.Sprite.__init__(self)
+		self.image = pg.image.load('enemybiu.png')
+		self.image = pg.transform.scale(self.image,(42,30))
+		self.x = redshipx
+		self.y = redshipy
+		self.speed = 30
+		self.rect = self.image.get_rect()
+		self.rect.x = redshipx
+		self.rect.y = redshipy
+
+	def update(self):
+		self.rect.x -= self.speed
+
+
+class Graybiu(pg.sprite.Sprite):
+
+	def __init__(self,y):
+		pg.sprite.Sprite.__init__(self)
+		self.image = pg.image.load('enemybiu.png')
+		self.image = pg.transform.scale(self.image,(42,30))
+		self.x = grayshipx
+		self.y = grayshipy
+		self.speed = 30
+		self.rect = self.image.get_rect()
+		self.rect.x = grayshipx
+		self.rect.y = grayshipy
+
+	def update(self):
+		self.rect.x -= self.speed
+
+
+class Fireball(pg.sprite.Sprite):
+
+	def __init__(self,y):
+		pg.sprite.Sprite.__init__(self)
+		self.image = pg.image.load('fireball.png')
+		self.image = pg.transform.scale(self.image,(300,150))
+		self.x =1450
+		self.y = firebally
+		self.speed = 15
+		self.rect = self.image.get_rect()
+		self.rect.x = 1450
+		self.rect.y = firebally
+
+	def update(self):
+		self.rect.x -= self.speed
 
 enemy = 0
+allsprite = pg.sprite.Group()
+crashing = pg.sprite.Group()
+# from here
+redshipy = random.randint(0,674)
+grayshipy = random.randint(0,660)
+firebally = random.randint(0,650)
 rockey = random.randint(0,740)
 meteoy = random.randint(0,700)
 rocy = random.randint(0,728)
-allsprite = pg.sprite.Group()
-crashing = pg.sprite.Group()
+redship = Redship(redshipy)
+grayship = Grayship(grayshipy)
+fireball = Fireball(firebally)
 rocket = Rocket(rockey)
 meteor = Meteor(meteoy)
 rock = Rock(rocy)
+crashing.add(redship)
+crashing.add(grayship)
 crashing.add(rocket)
 crashing.add(meteor)
 crashing.add(rock)
+crashing.add(fireball)
+allsprite.add(redship)
+allsprite.add(grayship)
 allsprite.add(rocket)
 allsprite.add(meteor)
 allsprite.add(rock)
+allsprite.add(fireball)
+# to here
 ship = Ship()
 allsprite.add(ship)
 allsprite.draw(screen)
@@ -113,18 +229,32 @@ while running:
 	clock.tick(60)
 	crash = pg.sprite.spritecollide(ship, crashing, True)
 	if enemy > 120:
+		# from here
+		redshipy = random.randint(0,674)
+		grayshipy = random.randint(0,660)
+		firebally = random.randint(0,650)
 		rockey = random.randint(0,740)
 		meteoy = random.randint(0,700)
 		rocy = random.randint(0,728)
+		redship = Redship(redshipy)
+		grayship = Grayship(grayshipy)
+		fireball = Fireball(firebally)
 		rocket = Rocket(rockey)
 		meteor = Meteor(meteoy)
 		rock = Rock(rocy)
+		crashing.add(redship)
+		crashing.add(grayship)
 		crashing.add(rocket)
 		crashing.add(meteor)
 		crashing.add(rock)
+		crashing.add(fireball)
+		allsprite.add(redship)
+		allsprite.add(grayship)
 		allsprite.add(rocket)
 		allsprite.add(meteor)
 		allsprite.add(rock)
+		allsprite.add(fireball)
+		# to here
 		enemy = 0		
 	enemy += 1
 	text = font.render('score:' + str(score), True, (212,201,106))
@@ -134,16 +264,21 @@ while running:
 	bg = pg.transform.scale(bg,(1400,800))
 	bg.blit(text,(130,10))
 	pg.display.update()
-	allsprite.update()
+	
 	keys = pg.key.get_pressed()
 	if keys[pg.K_s]:
 		if ship.rect.y <= 674:
 			Ship.movedown(ship)
-	elif keys[pg.K_w]:
+	if keys[pg.K_w]:
 		if ship.rect.y >= 0:
 			Ship.moveup(ship)
+	if keys[pg.K_SPACE]:
+		shipy = ship.rect.y
+		bullet = Bullet(shipy)
+		allsprite.add(bullet)
 	if crash:
 		running = False
+	allsprite.update()
 	screen.blit(bg, (0,0))
 	allsprite.draw(screen)
 	for event in pg.event.get():
